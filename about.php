b746e122d6c81afa1e48495b28bef41ba71c82e3
Tom Roche <tomroche24@gmail.com>
2024-06-23 22:33:17 +0200

    fix about hero

--- about.php
+++ about.php
@@ -6,8 +6,8 @@ include_once "includes/header.php";
     <div class="container">
         <div class="row">
             <div class="col-11 hero-inner">
-                <h2>About us</h2>
-                <p>
+                <h2 class="text-center text-sm-start">About us</h2>
+                <p class="text-center text-sm-start">
                     Coffeeroasters began its journey of exotic discovery in 1999, highlighting stories of coffee from around 
                     the world. We have since been dedicated to bring the perfect cup - from bean to brew - in every shipment.
                 </p>
