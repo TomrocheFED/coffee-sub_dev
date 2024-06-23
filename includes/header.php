523bdcea5c5fec2e6ff71ef2ee616c8f714297bb
Tom Roche <tomroche24@gmail.com>
2024-06-23 15:52:08 +0200

    Add header include

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-between py-5">
+                <div class="header-logo">
+                    <a href="#">
+                        <img src="assets/shared/desktop/logo.svg" alt="Logo">
+                    </a>
+                </div>
+                <button id="mm-open-menu" class="d-block d-md-none"></button>
+                <nav class="d-none d-md-block">
+                    <ul class="d-flex align-items-center gap-5">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Create your plan</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
--- includes/top.php
+++ includes/top.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="css/bs5/bootstrap.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/main.css">
 
     <title>Frontend Mentor Challenge || Coffee subscription</title>
 </head>
