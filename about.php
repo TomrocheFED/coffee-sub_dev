5e33b02dbaec8bda6b0d0709737e2a61aa78f07a
Tom Roche <tomroche24@gmail.com>
2024-06-25 20:25:46 +0200

    add commitment section

--- about.php
+++ about.php
@@ -15,6 +15,28 @@ include_once "includes/header.php";
         </div>
     </div>
 </section>
+
+<section id="commitment">
+    <div class="container">
+        <div class="row">
+            <div class="col-11">
+                <div class="row gap-5 gap-md-0">
+                    <div class="col-md-6 d-flex align-items-center justify-content-center justify-content-md-start">
+                        <img class="img-fluid" src="assets/about/desktop/image-commitment.jpg" alt="Man making a cappuccino.">
+                    </div>
+                    <div class="col-md-6 d-flex align-items-center">
+                        <div class="text-center text-md-start">
+                            <h2 class="mb-4">Our commitment</h2>
+                            <p>
+                            We’re built on a simple mission and a commitment to doing good along the way. We want to make it easy for you to discover and brew the world’s best coffee at home. It all starts at the source. To locate the specific lots we want to purchase, we travel nearly 60 days a year trying to understand the challenges and opportunities in each of these places. We collaborate with exceptional coffee growers and empower a global community of farmers through with well above fair-trade benchmarks. We also offer training, support farm community initiatives, and invest in coffee plant science. Curating only the finest blends, we roast each lot to highlight tasting profiles distinctive to their native growing region.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php 
 include_once "includes/footer.php";
 ?>
